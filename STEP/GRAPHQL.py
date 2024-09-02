4ddc73c9df53440cc4d03b46f78d2bdaffad6e24
David Spackman <davidjspackman@gmail.com>
2024-09-02 15:29:29 +1000

    Add some functionality to interact with graphql api

--- STEP/GRAPHQL.py
+++ STEP/GRAPHQL.py
@@ -3,14 +3,15 @@ import urllib.parse
 
 from gql import Client, gql
 from gql.transport.requests import RequestsHTTPTransport
+from graphql.language import print_ast
 from copy import copy
 from Argumental.Argue import Argue
 
 args = Argue()
 
 #====================================================================================================
-@args.command(name='step')
-class STEP(object):
+@args.command(name='graphqlbase')
+class GraphQLBase:
 	'''
 	base class to store the common properties and operations
 	'''
@@ -56,7 +57,7 @@ class STEP(object):
 		return result.text
 	
 	#________________________________________________________________________________________________
-	def auth(self):	
+	def get_token(self):
 		path='%s'%('auth')
 		body = {
 			"userId": self.username,
@@ -67,81 +68,168 @@ class STEP(object):
 			'content-type': 'application/x-www-form-urlencoded',
 		}
 		return self.post(path=path,body=encoded_body, headers=headers)
-	
+
+#====================================================================================================
+@args.command(name='graphql')
 class GraphQL:
+
+    @args.property(short='H', default='http://host')
+    def hostname(self): return
+
+    @args.property(short='T')
+    def token(self): return
+
+    @args.property(short='C', default='Context1')
+    def context(self): return
+
+    @args.property(short='W', default='Main')
+    def workspace(self): return
+
+    @args.property(short='v', flag=True)
+    def verbose(self): return
+
 	#________________________________________________________________________________________________
-    def __init__(self, graphql_url, token=None):
+    def __init__(self, verbose=None, hostname=None, context=None, workspace=None):
         # Set up the transport and client
-        self.transport = RequestsHTTPTransport(
-            url=graphql_url,
-            headers={"Authorization": f"Bearer {token}"} if token else {},
-            use_json=True,
+        if verbose: self.verbose = verbose
+        if hostname: self.hostname = hostname
+        if context: self.context=context
+        if workspace: self.workspace = workspace
+        self.use_json = True
+		
+        self.headers={
+		}
+		
+        self.path = 'graphqlv2/graphql'
+
+    def execute(self, query, params):
+        url = '%s/%s'%(self.hostname, self.path)
+        token = self.token
+		
+        headers = copy(self.headers)
+        headers['Authorization'] = f"Bearer {token}"
+			
+        transport = RequestsHTTPTransport(
+            url=url,
+            headers=headers,
+            use_json=self.use_json,
         )
-        self.client = Client(transport=self.transport, fetch_schema_from_transport=True)
+		
+        if self.verbose:
+            print(json.dumps({'url': url, 'headers': headers, 'params': params}, indent=4), file=sys.stderr)
+        
+        client = Client(transport=transport, fetch_schema_from_transport=True)
+        return client.execute(query, variable_values=params)
 
 #====================================================================================================
 class GraphQLQuery(GraphQL):
 
 	#________________________________________________________________________________________________
-    def get_books_by_author(self, author_name):
-        query = gql("""
-        query ($authorName: String!) {
-            author(name: $authorName) {
-                name
-                books {
-                    title
-                }
-            }
-        }
+    def get_product(self, id, fields=None, value_fields=None):
+        # Default fields if none are provided
+        if fields is None:
+            fields = ["id", "name"]
+            # fields = ["uniqueId", "id", "name", "title", "approvalStatus", 
+            #       "approvalStatusName", "currentRevision", 
+            #       "currentRevisionLastEdited", "hasChildren"]
+
+        
+        # Default value fields if none are provided
+        if value_fields is None:
+            value_fields = ["simpleValue", "attribute { id name}"]
+            # value_fields = ["inherited", "editable", "simpleValue", "calculated", 
+            #                 "mandatoryForApproval", "attribute { id name title multivalued listOfValuesBased calculated specification }"]
+
+        # Join fields into the query string
+        query_fields = "\n".join(fields)
+        
+        # Join value fields into the query string
+        value_query_fields = "\n".join(value_fields)
+
+        query = gql(f"""
+        query ($context: String!, $workspace: String!, $id: String!) {{
+            product(context: $context, workspace: $workspace, id: $id) {{
+                {query_fields}
+                values(attributes: null) {{
+                    {value_query_fields}
+                }}
+            }}
+        }}
         """)
-        params = {"authorName": author_name}
-        response = self.client.execute(query, variable_values=params)
-        return response['author']
+
+        params = {
+            "id": id,
+            "context" : self.context,
+            "workspace" : self.workspace
+        }
+        response = self.execute(query, params=params)
+        return response['product']
 
 	#________________________________________________________________________________________________
-    def get_authors_by_book_title(self, book_title):
-        query = gql("""
-        query ($bookTitle: String!) {
-            book(title: $bookTitle) {
-                title
-                author {
-                    name
-                }
-            }
-        }
-        """)
-        params = {"bookTitle": book_title}
-        response = self.client.execute(query, variable_values=params)
-        return response['book']['author']
+    # def get_authors_by_book_title(self, book_title):
+    #     query = gql("""
+    #     query ($bookTitle: String!) {
+    #         book(title: $bookTitle) {
+    #             title
+    #             author {
+    #                 name
+    #             }
+    #         }
+    #     }
+    #     """)
+    #     params = {"bookTitle": book_title}
+    #     response = self.client.execute(query, variable_values=params)
+    #     return response['book']['author']
 
 
 #====================================================================================================
 class GraphQLMutation(GraphQL):
-	#________________________________________________________________________________________________
-    def add_book(self, title, author_name):
-        mutation = gql("""
-        mutation ($title: String!, $authorName: String!) {
-            addBook(title: $title, authorName: $authorName) {
-                title
-                author {
-                    name
-                }
-            }
-        }
-        """)
-        params = {"title": title, "authorName": author_name}
-        response = self.client.execute(mutation, variable_values=params)
-        return response['addBook']
 
 	#________________________________________________________________________________________________
-    def add_author(self, name):
-        mutation = gql("""
-        mutation ($name: String!) {
-            addAuthor(name: $name) {
-                name
-            }
-        }
+    def execute_action(self, id, node_type, action_id, fields=None, error_fields=None):
+    
+        # Default fields if none are provided
+        if fields is None:
+            fields = ["id", "title"]
+        
+        # Default error fields if none are provided
+        if error_fields is None:
+            error_fields = ["path", "message", "dataIssuesReport { header }"]
+
+        # Join fields into the query string
+        fields_query = "\n".join(fields)
+        
+        # Join error fields into the query string
+        error_fields_query = "\n".join(error_fields)
+
+        mutation = gql(f"""
+        mutation ($context: String!, $workspace: String!, $node: String!, $action: String!) {{
+            executeAction(
+                context: $context,
+                workspace: $workspace,
+                input: {{
+                    node: $node,
+                    nodeType: {node_type},
+                    action: $action
+                }}
+            ) {{
+                success
+                errors {{
+                    {error_fields_query}
+                }}
+                node {{
+                    {fields_query}
+                }}
+            }}
+        }}
         """)
-        params = {"name": name}
-        response = self.client.execute(mutation, variable_values=params)
-        return response['addAuthor']
+
+        params = {
+            "context": self.context,
+            "workspace": self.workspace,
+            "node": id,
+            "action": action_id
+        }
+        
+        response = self.execute(mutation, params=params)
+        return response['executeAction']
